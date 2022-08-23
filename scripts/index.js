b6350224a2fad51bf735b796df44a6faf635f9ab
sanjayrathva091 <sanjayrathva091@gmail.com>
2022-08-24 00:48:35 +0530

    added navbar header features

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+
+
+let latestUpdate = document.querySelector('#new_update > div #close_symbol');
+latestUpdate.addEventListener('click', () => {
+    document.getElementById('new_update').innerHTML= null;
+});
+console.log(latestUpdate);
