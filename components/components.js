7539a4f55a72762599677d2dd1b038a4b5d92e88
sanjayrathva091 <sanjayrathva091@gmail.com>
2022-08-26 23:43:44 +0530

    modifiled css properties

---
+++ components/components.js
@@ -0,0 +1,58 @@
+function myfooter() {
+    return `<div class="container">
+    <h2 class="brand -dark"><a href="/"></a>
+      <p>WORKOUT COMPLETE<sup>™</sup></p>
+    </h2>
+    
+    <nav class="footer__nav alt-nav">
+      <ul>
+          <li><a class="demi" href="#">Workouts</a></li>
+          <li><a href="#">Workout Videos</a></li>
+          <li><a href="#">Custom Workouts</a></li>
+          <li><a class="demi" href="#">Programs</a></li>
+          <li><a href="#">Workout Programs</a></li>
+          <li><a href="#">Meal Plans</a></li>
+          <li><a href="#">Pilot Programs</a></li>
+          <li><a href="#">Routines</a></li>
+      </ul>
+      <ul>
+        <li><a class="demi" href="#">Healthy Living</a></li>
+        <li><a href="#">Fitness</a></li>
+        <li><a href="#">Health</a></li>
+        <li><a href="#">Nutrition</a></li>
+        <li><a href="#">Healthy Recipes</a></li>
+        <li><a href="#">Experts</a></li>
+      </ul>
+      <ul>
+        <li><a class="demi" href="#">About</a></li>
+        <li><a href="#">Careers</a></li>
+        <li><a href="#">Tutorials</a></li>
+        <li><a href="#">Our Team</a></li>
+        <li><a href="#">B2B Options</a></li>
+      </ul>
+      <ul>
+        <li><a class="demi" href="#">Membership</a></li>
+        <li><a class="demi" href="#">FB Plus</a></li>
+        <li><a class="demi" href="#">Community</a></li>
+        <li><a class="demi" href="#">Blog</a></li>
+        <li><a class="demi" href="#">Contact Us</a></li>
+        <li><a class="demi" href="#">FAQ</a></li>
+        <li><a class="demi" href="#">Store </a></li>
+      </ul>
+    </nav>
+  </div>
+  <div class="container">
+    <div class="end">
+      <p>Copyright © 2022 Fitness Blender. All rights reserved. <a href="#">Terms of Use</a> <a href="#">Privacy Policy</a></p>
+      <ul class="nav-smicons footer-smicons">
+        <li class="youtube-smi"><a href="#"><span class="iconfont-youtube"></span><span><i class="fa-brands fa-youtube"></i></span></a></li>
+        <li class="pinterest-mi"><a href="#"><span class="iconfont-pinterest"></span><span><i class="fa-brands fa-pinterest-p"></i></span></a></li>
+        <li class="facebook-smi"><a href="#"><span class="iconfont-facebook"></span><span><i class="fa-brands fa-facebook-f"></i></span></a></li>
+        <li class="instagram-smi"><a href="#"><span class="iconfont-instagram"></span><span><i class="fa-brands fa-instagram"></i></span></a></li>
+        <li class="twitter-smi"><a href="#"><span class="iconfont-twitter"></span><span><i class="fa-brands fa-twitter"></i></span></a></li>
+      </ul>
+    </div>
+  </div>`;
+}
+
+export { myfooter };
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,10 @@
-
+import { myfooter  } from "../components/components.js";
 
 let latestUpdate = document.querySelector('#new_update > div #close_symbol');
 latestUpdate.addEventListener('click', () => {
     document.getElementById('new_update').innerHTML= null;
 });
 console.log(latestUpdate);
+
+let footer = document.querySelector('main+footer');
+footer.innerHTML = myfooter();
