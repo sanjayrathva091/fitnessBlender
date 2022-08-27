6f34b2206efd9aaf0c5bb6d1baf4846d276e8ef5
sanjayrathva091 <sanjayrathva091@gmail.com>
2022-08-27 14:40:12 +0530

    modified header import

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,13 @@
-import { myfooter  } from "../components/components.js";
+import { myheader, myfooter  } from "../components/components.js";
 
 let latestUpdate = document.querySelector('#new_update > div #close_symbol');
 latestUpdate.addEventListener('click', () => {
     document.getElementById('new_update').innerHTML= null;
 });
 console.log(latestUpdate);
+
+let header = document.querySelector('#navbar');
+header.innerHTML = myheader();
 
 let footer = document.querySelector('main+footer');
 footer.innerHTML = myfooter();
