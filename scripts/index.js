fb6c71a451ddb1af8ff520d2093724ec3e15df17
sanjayrathva091 <sanjayrathva091@gmail.com>
2022-08-27 14:41:27 +0530

    modified header import

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ let latestUpdate = document.querySelector('#new_update > div #close_symbol');
 latestUpdate.addEventListener('click', () => {
     document.getElementById('new_update').innerHTML= null;
 });
-console.log(latestUpdate);
+// console.log(latestUpdate);
 
 let header = document.querySelector('#navbar');
 header.innerHTML = myheader();
